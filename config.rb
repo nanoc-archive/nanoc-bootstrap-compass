d3e6aef8f3ccd0e3770cfd39696a63036ec4a182
Denis Defreyne <denis.defreyne@stoneship.org>
2010-06-29 09:22:08 +0200

    renamed compass_config.rb to the more standard config.rb

--- config.rb
+++ config.rb
@@ -1,3 +1,6 @@
+# This is the Compass configuration file. The configuration file for nanoc is 
+# named “config.yaml”.
+
 project_path = File.dirname(__FILE__)
 http_path    = '/'
 output_style = :compressed
